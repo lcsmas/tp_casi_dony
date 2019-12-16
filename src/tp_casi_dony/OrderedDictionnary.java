6ce19f9bfd1968e03b2b869e5f777aefe1a59fa2
Lucas <lucas_mas@msn.com>
2019-12-16 16:32:03 +0100

    Fast / Sorted

--- src/tp_casi_dony/OrderedDictionnary.java
+++ src/tp_casi_dony/OrderedDictionnary.java
@@ -3,9 +3,9 @@ package tp_casi_dony;
 public class OrderedDictionnary extends AbstractDictionnary {
 
 	@Override
-	public int newIndexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int newIndexOf(Object o) {	
+		grow();
+		return super.key.length-1;
 	}
 
 	@Override
@@ -18,6 +18,15 @@ public class OrderedDictionnary extends AbstractDictionnary {
 			}
 		}
 		return res;
-	}
+	}	
 	
+	protected void grow() {
+		int newSize = super.key.length + 1;
+		Object[] newKeyArray = new Object[newSize];
+		Object[] newValueArray = new Object[newSize];
+		System.arraycopy(super.key, 0, newKeyArray, 0, newSize-1);
+		System.arraycopy(super.value, 0, newValueArray, 0, newSize-1);
+		super.key = newKeyArray;
+		super.value = newValueArray;
+	}
 }
